4908fdc554ffa11fc261ebb646864873b3573ddb
fibafathima <fibaaminnu@gmail.com>
2026-03-07 15:35:19 +0530

    feat: Add Google Maps API Key to Android, iOS, and Web

--- plantconnect/ios/Runner/AppDelegate.swift
+++ plantconnect/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_API_KEY_HERE")
+    GMSServices.provideAPIKey("AIzaSyAc4vGStxbJVVazvBGiwUZqhwSuMKOHoy8")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
